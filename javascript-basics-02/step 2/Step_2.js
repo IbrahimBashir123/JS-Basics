929d62c80346ee40570913b4ee6f2711ee6c493e
Ibrahim Bachir <Ibrahimbashir11111@gmail.com>
2022-12-08 21:38:58 +0200

    Basic-2 Step-2

---
+++ javascript-basics-02/step 2/Step_2.js
@@ -0,0 +1,10 @@
+var inputname = document.querySelector("#name");
+inputname.addEventListener("blur", function () {
+  alert(`Thank you for participating!`);
+});
+
+// function message() {
+//     document.getElementById("name").onblur=message;
+//     var text = document.getElementById("name").value;
+//     alert(text);
+// }
