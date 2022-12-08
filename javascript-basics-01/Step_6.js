ff4a5a8ea9d8e66bbe50a43b5104bd76059fa4d4
Ibrahim Bachir <Ibrahimbashir11111@gmail.com>
2022-12-08 12:10:21 +0200

    Step 6 : External files and Simple Math

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,15 @@
+document.getElementById("validate").onclick = function () {
+  var numOne = document.getElementById("first_number").value;
+  var numTwo = document.getElementById("second_number").value;
+  var product = parseInt(numOne) % parseInt(numTwo);
+
+  window.prompt("Do u want to make a Divide?");
+  alert(
+    "first_number: " +
+      numOne +
+      "\nsecond_number: " +
+      numTwo +
+      "\nproduct: " +
+      product
+  );
+};
