ed0c33a751dcf778724dc56b46dc3b6da20c576e
Ibrahim Bachir <Ibrahimbashir11111@gmail.com>
2022-12-09 14:51:49 +0200

    Basics 3 - Style Manipulation

---
+++ javascript-basics-03/step 2/Step_2.js
@@ -0,0 +1,25 @@
+// let showText = document.querySelector("#show");
+// let hideText = document.querySelector("#hide");
+// let text = document.querySelector("#texte");
+
+// function StyleManipulation() {
+//   showText.onclick = function () {
+//     texte.style.display = "inline";
+//   };
+//   hideText.onclick = function () {
+//     text.style.display = "none";
+//   };
+// }
+// StyleManipulation();
+
+var button = document.querySelectorAll(`a`);
+var paragraph = document.querySelectorAll(`texte`);
+button.forEach((showandhide) => {
+  showandhide.addEventListener(`click`, (showandhide) => {
+    if (showandhide.target.id == `hide`) {
+      texte.style = `display: none`;
+    } else {
+      texte.style = `display:block`;
+    }
+  });
+});
